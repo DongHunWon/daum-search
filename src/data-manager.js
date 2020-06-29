d359c8f1b6781e2a94e92fc61c3b82eae6df1ecd
DongHunWon <37091640+DongHunWon@users.noreply.github.com>
2020-06-29 16:28:26 +0900

    fetch api

---
+++ src/data-manager.js
@@ -0,0 +1,10 @@
+function trendingList(path, page) {
+  return fetch(
+    `http://1boon.kakao.com/ch/${path}.json?pagesize=10&page=${page}`,
+    {
+      method: 'get',
+    },
+  ).then((res) => res.json());
+}
+
+export { trendingList };
